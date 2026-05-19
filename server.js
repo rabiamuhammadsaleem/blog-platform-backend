b99ac90aefbec60a2535d5880f00a7a77b60ff19
rabiamuhammadsaleem <rabiamuhammadsaleem2307@gmail.com>
2026-05-20 01:02:57 +0500

    commit

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+// app.use(cors({ origin: ['http://localhost:3000', 'https://blog-platform-backend-production-2df3.up.railway.app'], credentials: true }));
+app.use(cors({ 
+    origin: ['http://localhost:3000', 'https://blog-platform-backend-production-2df3.up.railway.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
