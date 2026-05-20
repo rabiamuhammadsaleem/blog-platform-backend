a2bf33cc37630f5c4f87248fa2d324fe2da2ae39
rabiamuhammadsaleem <rabiamuhammadsaleem2307@gmail.com>
2026-05-20 21:43:35 +0500

    commit

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(cors({
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'X-Requested-With']
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', ]
 }));
 
 // Handle preflight requests
